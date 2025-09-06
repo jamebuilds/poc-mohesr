eb1d2c5e38057e71cc82be5d85c2cba84ef94c31
Jame Ng <jamebuilds.com>
2025-09-06 14:07:56 +0800

    wip

--- resources/js/pages/upload.tsx
+++ resources/js/pages/upload.tsx
@@ -11,10 +11,8 @@ import { Form, Head } from '@inertiajs/react';
 export default function Upload() {
     return (
         <>
-            <Head title="Upload Credentials">
-                <link rel="preconnect" href="https://fonts.bunny.net" />
-                <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-            </Head>
+            <Head title="Upload Credentials" />
+                
             <div className="p-10">
                 <Heading title="POC - MOHESR" />
                 <Form className="space-y-4" action={store()} resetOnSuccess>
