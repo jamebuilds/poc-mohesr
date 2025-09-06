5cbee155890af311ed1078a6ba4c09bc19eb9a68
Jame Ng <jamebuilds.com>
2025-09-06 13:31:37 +0800

    wip

---
+++ resources/js/pages/upload.tsx
@@ -0,0 +1,20 @@
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Head } from '@inertiajs/react';
+
+export default function Upload() {
+    return (
+        <>
+            <Head title="Upload Credentials">
+                <link rel="preconnect" href="https://fonts.bunny.net" />
+                <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+            </Head>
+            <div className="p-10">
+                <div className="grid w-full max-w-sm items-center gap-3">
+                    <Label htmlFor="credential">Upload a Credential</Label>
+                    <Input id="credential" type="file" required name="credential" />
+                </div>
+            </div>
+        </>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    return redirect(route('upload'));
 })->name('home');
+
+Route::get('/upload', UploadController::class)->name('upload');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
@@ -13,5 +16,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
