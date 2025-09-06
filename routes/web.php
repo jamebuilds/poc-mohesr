f58c9edf7a9375f5e0453e6c63315979309403c3
Jame Ng <jamebuilds.com>
2025-09-06 13:32:00 +0800

    wip

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
+// todo: disable the routes?
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
